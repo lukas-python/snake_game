b159e26dabc4450519cc797e42ee77d22c195fa1
lukas-python <72907442+lukas-python@users.noreply.github.com>
2022-03-23 18:20:50 +0100

    Create scoreboard.py

---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+ALIGNMENT = 'center'
+FONT = ('Courier', 20, 'normal')
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.hideturtle()
+        self.goto(0, 280)
+        self.color('white')
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increse_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
